1dfa46bdb1ab92eb94eeaf27050ce864ebe6f1ef
unknown <dajara@gmail.com>
2023-10-12 15:41:00 -0500

    Validacion de rol

--- routes/validateRol.py
+++ routes/validateRol.py
@@ -10,16 +10,17 @@ def verificarUsuario(rolRequerido):
             # Si el usuario tiene el rol requerido, puedes acceder a sus atributos en tu vista
             idRolUsuario = session.get('idUsuarioRolFK')
             usuario = Usuario.query.get(idRolUsuario)  # Consulta la base de datos para obtener al usuario
-            if usuario == idRolUsuario:
-                return True
+            if usuario is not None:
+                return True  # Usuario con el rol requerido encontrado
             else:
-                return "Usuario no encontrado", 404
+                return "Usuario no encontrado", 404  # Devuelve un mensaje de error 404
         else:
             # Manejar el caso en el que el usuario no tiene el rol requerido
-            return "Acceso no autorizado", 403  # Devuelve un código de estado 403 (Prohibido), por ejemplo
+            return "Acceso no autorizado", 403  # Devuelve un mensaje de error 403 (Prohibido)
     else:
         # Manejar el caso en el que el rol de usuario no está en la sesión
-        return "Usuario no autenticado", 401  # Devuelve un código de estado 401 (No autorizado), por ejemplo
+        return "Usuario no autenticado", 401  # Devuelve un mensaje de error 401 (No autorizado)
+
 
 #Decoradores Personalizados
 
@@ -27,7 +28,7 @@ def verificarUsuario(rolRequerido):
 def usuarioAdminRequired(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if verificarUsuario(1):  # 1 es el rol del administrador
+        if 'rol' in session and session['rol'] == 1:  # 1 es el rol del administrador
             return func(*args, **kwargs)
         else:
             abort(403)  # Acceso prohibido para otros roles
@@ -37,7 +38,7 @@ def usuarioAdminRequired(func):
 def usuarioClientRequired(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if verificarUsuario(2):  # 2 es el rol del cliente
+        if 'rol' in session and session['rol'] == 2:  # 2 es el rol del cliente
             return func(*args, **kwargs)
         else:
             abort(403)  # Acceso prohibido para otros roles
